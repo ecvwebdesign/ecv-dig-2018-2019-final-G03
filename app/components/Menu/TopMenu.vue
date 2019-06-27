e17390575aeecf22a57db34d3bf33db0b3eed413
Mathieu33260 <mat_dumez@hotmail.fr>
2019-06-27 11:20:04 +0200

    close confirm

--- app/components/Menu/TopMenu.vue
+++ app/components/Menu/TopMenu.vue
@@ -3,11 +3,14 @@
         <StackLayout orientation="horizontal">
             <Image v-if="headerHasImage" src="~/assets/images/intersport.png" width="40%" marginRight="80" marginLeft="100"/>
             <Label v-else :text="headerLabel" textAlignment="center" width="40%" fontSize="16" marginRight="80" marginLeft="100" textWrap="true"/>
-            <SvgImage v-if="nbProductCart === 1" src="~/assets/icons/panier-1.svg" width="60px" @tap="changePage('cart')"/>
-            <SvgImage v-if="nbProductCart === 2" src="~/assets/icons/panier-2.svg" width="60px" @tap="changePage('cart')"/>
-            <SvgImage v-if="nbProductCart === 3" src="~/assets/icons/panier-3.svg" width="60px" @tap="changePage('cart')"/>
-            <SvgImage v-if="nbProductCart === 4" src="~/assets/icons/panier-4.svg" width="60px" @tap="changePage('cart')"/>
-            <Image v-if="nbProductCart === 0" src="~/assets/icons/panier.png" width="6%" @tap="changePage('cart')"/>
+            <template v-if="currentPage !== 'confirmpay'">
+                <SvgImage v-if="nbProductCart === 1" src="~/assets/icons/panier-1.svg" width="60px" @tap="changePage('cart')"/>
+                <SvgImage v-if="nbProductCart === 2" src="~/assets/icons/panier-2.svg" width="60px" @tap="changePage('cart')"/>
+                <SvgImage v-if="nbProductCart === 3" src="~/assets/icons/panier-3.svg" width="60px" @tap="changePage('cart')"/>
+                <SvgImage v-if="nbProductCart === 4" src="~/assets/icons/panier-4.svg" width="60px" @tap="changePage('cart')"/>
+                <Image v-if="nbProductCart === 0" src="~/assets/icons/panier.png" width="6%" @tap="changePage('cart')"/>
+            </template>
+            <SvgImage v-else src="~/assets/icons/croix.svg" width="60px" @tap="changePage('home')"/>
         </StackLayout>
     </ActionBar>
 </template>
@@ -29,6 +32,9 @@
             },
             nbProductCart() {
                 return this.$store.state.cart.length;
+            },
+            currentPage() {
+                return this.$store.state.currentPage;
             }
         }
     }
